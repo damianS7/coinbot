b2086407df94ea222cf40f4c7655caedc7cc8b4d
danjian <josepwnz@gmail.com>
2016-10-22 22:48:55 +0200

    restart

--- src/main/java/com/coinbot/core/CoinbotApplication.java
+++ src/main/java/com/coinbot/core/CoinbotApplication.java
@@ -16,116 +16,11 @@
  */
 package com.coinbot.core;
 
-import java.awt.EventQueue;
-import java.io.File;
 import java.io.IOException;
-import java.util.logging.Logger;
-
-import com.coinbot.database.AddressDatabase;
-import com.coinbot.database.AntibotDatabase;
-import com.coinbot.database.CaptchaDatabase;
-import com.coinbot.database.FaucetDatabase;
-import com.coinbot.faucet.Stats;
-import com.coinbot.ui.UI;
 
 public class CoinbotApplication {
-	private static final Logger LOGGER = Logger.getLogger(CoinbotApplication.class
-			.getName());
-	public static final String dirName = "coinbot/";
-	public static CoinbotProperties coinbotProperties;
-	public static AddressDatabase addressDatabase;
-	public static FaucetDatabase faucetDatabase;
-	public static CaptchaDatabase captchaDatabase;
-	public static AntibotDatabase antibotDatabase;
-	public static Stats stats;
-	public static Coinbot bot;
-	public static UI ui;
-	
+		
 	public static void main(String[] args) throws IOException {
-		LOGGER.info("Comprobando permisos de escritura ...");
-		File pwd = new File(".");
-		if(!pwd.canWrite()) {
-			LOGGER.severe("No se puede escribir en " + pwd.getAbsolutePath());
-			return;
-		}
 		
-		LOGGER.info("Preparando ficheros necesarios para la aplicacion ...");
-		File dir = new File(dirName);
-		if(!dir.exists()) {
-			dir.mkdir();
-		}
-		
-		File bitcoinAddresses = new File(dirName + "btcaddresses.txt");
-		if(!bitcoinAddresses.exists()) {
-			try {
-				bitcoinAddresses.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-				return;
-			}
-		}
-		
-		File captcha = new File(dirName + "captchas.txt");
-		if(!captcha.exists()) {
-			try {
-				captcha.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-				return;
-			}
-		}
-		
-		File faucets = new File(dirName + "faucets.txt");
-		if(!faucets.exists()) {
-			try {
-				faucets.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-				return;
-			}
-		}
-
-		File properties = new File(dirName + "coinbot.properties");
-		if(!properties.exists()) {
-			try {
-				properties.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-				return;
-			}
-		}
-		
-		File antibot = new File(dirName + "antibotpuzzle.json");
-		if(!antibot.exists()) {
-			try {
-				antibot.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-				return;
-			}
-		}
-		
-		stats = new Stats();
-		addressDatabase = new AddressDatabase(bitcoinAddresses);
-		LOGGER.info("Direcciones btc cargadas: " + addressDatabase.load());
-		
-		faucetDatabase = new FaucetDatabase(faucets);
-		LOGGER.info("Faucets cargadas: " + faucetDatabase.load());
-		
-		captchaDatabase = new CaptchaDatabase(captcha);
-		LOGGER.info("Captchas cargados: " + captchaDatabase.load());
-		
-		antibotDatabase = new AntibotDatabase(antibot);
-		LOGGER.info("Antibots cargados: " + antibotDatabase.load());
-		
-		LOGGER.info("Cargando configuracion de la aplicacion");
-		coinbotProperties = new CoinbotProperties(antibot);
-		bot = new Coinbot();
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				ui = new UI();
-			}
-		});
 	}
 }
