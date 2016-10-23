d4fa9f84f74e162088d39e1e966a75d62af2ba98
danjian <josepwnz@gmail.com>
2016-10-23 23:43:55 +0200

    ...

--- src/main/java/com/coinbot/detector/ClaimDetector.java
+++ src/main/java/com/coinbot/detector/ClaimDetector.java
@@ -16,6 +16,67 @@
  */
 package com.coinbot.detector;
 
-public class ClaimDetector {
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.coinbot.exceptions.DetectionException;
+import com.coinbot.exceptions.InvalidAddressException;
+import com.coinbot.exceptions.InvalidAntibotException;
+import com.coinbot.exceptions.InvalidCaptchaException;
+
+public class ClaimDetector implements Detector {
+	private WebDriver driver;
+	private boolean success = false;
+	
+	public ClaimDetector(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public boolean isClaimSuccessfull() {
+		return success;
+	}
+
+	@Override
+	public void detect() throws DetectionException, InvalidAntibotException,
+			InvalidCaptchaException, InvalidAddressException {
+		try {
+			List<WebElement> alertsDanger = driver.findElements(By.className("alert-danger"));
+			for (WebElement e : alertsDanger) {
+
+				if (e.getText().toLowerCase().contains("invalid captcha")) {
+					throw new InvalidCaptchaException("Invalid captcha.");
+				}
+
+				if (e.getText().toLowerCase()
+						.contains("invalid antibot")) {
+					throw new InvalidAntibotException(
+							"Invalid antibot verification.");
+				}
+
+				if (e.getText().toLowerCase().contains("invalid address")) {
+					throw new InvalidAddressException("Invalid address.");
+				}
+			}
+		} catch (NoSuchElementException e) {
+		}
+
+		try {
+			List<WebElement> alerts = driver.findElements(By.className("alert-success"));
+			for (WebElement e : alerts) {
+
+				if (e.getText().toLowerCase().contains("satoshi was sent")) {
+					this.success = true;
+					return;
+				}
+				
+			}
+		} catch (NoSuchElementException e) {
+		}
+
+		throw new DetectionException("Couldn't detect any output message.");
+	}
 }
