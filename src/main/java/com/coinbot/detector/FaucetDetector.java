8f2e3a51f617e786cbcae6f1bc59ec5c68623ce0
danjian <josepwnz@gmail.com>
2016-10-23 23:30:27 +0200

    ...

--- src/main/java/com/coinbot/detector/FaucetDetector.java
+++ src/main/java/com/coinbot/detector/FaucetDetector.java
@@ -16,6 +16,119 @@
  */
 package com.coinbot.detector;
 
-public class FaucetDetector {
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.coinbot.exceptions.DetectionException;
+
+public class FaucetDetector implements Detector {
+	private WebDriver driver;
+	private int balance = 0;
+	private int reward = 0;
+	private int timer = 30;
+	
+	public FaucetDetector(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	private void parseBalance(String[] s) {
+		for (int i = 0; i < s.length; i++) {
+			try {
+				balance = Integer.parseInt(s[i]);
+				return;
+			} catch (NumberFormatException ex) {
+				continue;
+			}
+		}
+	}
+	
+	private void parseReward(String[] s) {
+		for (int i = 0; i < s.length; i++) {
+			try {
+				reward = Integer.parseInt(s[i]);
+				return;
+			} catch (NumberFormatException ex) {
+				continue;
+			}
+		}
+	}
+
+	private void parseTimer(String[] s) {
+		for (int i = 0; i < s.length; i++) {
+			if(s[i].equals("minutes.")) {
+				try {
+					timer = Integer.parseInt(s[i-1]);
+				} catch (NumberFormatException ex) {
+				}
+			}
+		}
+	}
+	
+	public boolean isClaimeable() {
+		if(getBalance() > 10000 && reward > 10 && timer > 5) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public int getReward() {
+		return reward;
+	}
+	
+	public int getTimer() {
+		return timer;
+	}
+	
+	public String printData() {
+		String data = "";
+		data += "Balance: " + getBalance();
+		data += " Reward: " + getReward();
+		data += " Timer: " + getTimer();
+		data += " Claimeable: " + isClaimeable();
+		System.out.println(data);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return data;
+	}
+	
+
+	@Override
+	public void detect() throws DetectionException {
+		try {
+			List<WebElement> alerts = driver.findElements(By.className("alert"));
+			for (WebElement alert : alerts) {
+				String[] s = alert.getText().split(" ");
+				if(alert.getText().toLowerCase().contains("minutes.")) {
+					parseTimer(s);
+				}
+				
+				if(alert.getText().toLowerCase().contains("satoshi")) {
+					parseReward(s);
+				}
+				
+				if(alert.getText().toLowerCase().contains("balance")) {
+					parseBalance(s);
+				}
+			}
+		} catch (NoSuchElementException e) {
+			
+		}
+		
+		throw new DetectionException("Faucet data not detected!");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
