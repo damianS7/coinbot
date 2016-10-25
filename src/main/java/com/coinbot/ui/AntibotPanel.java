6adae399b392eb3d4cf146a82e2e849af1c99e1f
danjian <josepwnz@gmail.com>
2016-10-25 12:33:09 +0200

    ...

--- src/main/java/com/coinbot/ui/AntibotPanel.java
+++ src/main/java/com/coinbot/ui/AntibotPanel.java
@@ -16,6 +16,67 @@
  */
 package com.coinbot.ui;
 
-public class AntibotPanel {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+
+import com.coinbot.antibot.Antibot;
+
+public class AntibotPanel extends JPanel {
+	private static final long serialVersionUID = 1868852228497027988L;
+	private Antibot antibot;
+	private JTextField input;
+	private JLabel timer;
+	
+	public AntibotPanel(Antibot ab) {
+		this.antibot = ab;
+		setLayout(new MigLayout("", "[grow,center]", "[][]"));
+		
+		JLabel label = new JLabel();
+		label.setIcon(new ImageIcon(ab.getImage()));
+		add(label, "cell 0 0");
+		
+		input = new JTextField();
+		input.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+				}
+			}
+		});
+		JPanel panel = new JPanel();
+		add(panel, "cell 0 1,growx");
+		panel.setLayout(new MigLayout("", "[114px,grow][20px,center]", "[19px]"));
+		
+		panel.add(input, "cell 0 0,growx,aligny top");
+		input.setColumns(10);
+		
+		timer = new JLabel("-");
+		panel.add(timer, "cell 1 0,alignx center,aligny center");
+	}
+	
+	public Antibot getAntibot() {
+		return antibot;
+	}
+	
+	public void setTimer(int t) {
+		timer.setText(Integer.toString(t));
+	}
 }
